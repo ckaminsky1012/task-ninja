df6fcf82e6ebfecb3d3c7e2b317e2c1c350ce087
ckaminsky1012 <corey.kaminsky@gmail.com>
2015-04-19 12:36:08 -0400

    Added toaster

--- scripts/app.js
+++ scripts/app.js
@@ -2,37 +2,37 @@
 
 var app = angular
   .module('TaskNinjaApp', [
-    'ngAnimate',
-    'ngResource',    
+    'ngAnimate',    
+    'ngResource',
     'ngRoute',    
-    'firebase'
+    'firebase',
+    'toaster'
   ])
-  .constant('FURL', 'https://task-ninja-1012.firebaseio.com/')  
+  .constant('FURL', 'https://task-ninja.firebaseio.com/')  
   .config(function ($routeProvider) {
     $routeProvider      
       .when('/', {
-        templateUrl: 'views/main.html'        
+        templateUrl: 'views/main.html'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'     
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthController'
       })
       .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'AuthController'
-      })
-       .when('/register', {
-        templateUrl: 'views/register.html',
-        controller: 'AuthController'
-
-      })
-      .when('/post', {
-        templateUrl: 'views/post.html', 
-        controller: 'TaskController'       
-      })
-      .when('/edit/:taskId', {
-        templateUrl: 'views/edit.html',
-        controller: 'TaskController'       
-      })
-      .when('/browse', {
-        templateUrl: 'views/browse.html',
-        controller: 'TaskController'        
       })
       .otherwise({
         redirectTo: '/'
