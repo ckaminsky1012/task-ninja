e28fc968aa06e86f0b3b8b224807b9cd3b6eec62
ckaminsky1012 <corey.kaminsky@gmail.com>
2015-04-22 22:52:37 -0400

    Add the ability to edit and cancel a task

--- scripts/services/task.js
+++ scripts/services/task.js
@@ -1,35 +1,33 @@
 'use strict';
 
-app.factory('Task', function(FURL, $firebase, Auth){
-
+app.factory('Task', function(FURL, $firebase, Auth) {
 	var ref = new Firebase(FURL);
-	var tasks = new $firebase(ref.child('task')).$asArray();
+	var tasks = $firebase(ref.child('task')).$asArray();
 	var user = Auth.user;
 
 	var Task = {
-
 		all: tasks,
 
 		getTask: function(taskId) {
 			return $firebase(ref.child('task').child(taskId));
 		},
-
+				
 		createTask: function(task) {
 			task.datetime = Firebase.ServerValue.TIMESTAMP;
 			return tasks.$add(task);
 		},
 
 		editTask: function(task) {
-			var t = this.getTask(task.$id);
+			var t = this.getTask(task.$id);			
 			return t.$update({title: task.title, description: task.description, total: task.total});
 		},
 
-		cancelTask: function(taskId){
+		cancelTask: function(taskId) {
 			var t = this.getTask(taskId);
 			return t.$update({status: "cancelled"});
 		},
 
-		isCreator: function(task) {
+		isCreator: function(task) {			
 			return (user && user.provider && user.uid === task.poster);
 		},
 
